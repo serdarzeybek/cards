4debf6453dee44362f18461934f886ecd6ca214d
serdarz-at-160046759299 <serdar.zeybek@gmail.com>
2018-09-07 03:00:02 +0300

    klkl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ElementService} from './element/element.service';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cards';
+  flips: boolean[] = [];
+  icons: string[];
+
+  constructor(private elementService: ElementService) {
+
+  }
+
+  ngOnInit() {
+    this.icons = this.elementService.getIcons();
+    for (const index in this.icons) {
+      this.flips[index] = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FlipModule} from 'ngx-flip';
+import { ElementComponent } from './element/element.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatGridListModule} from '@angular/material';
+import {ElementService} from './element/element.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlipModule,
+    FlexLayoutModule,
+    MatGridListModule
   ],
-  providers: [],
+  exports: [
+    MatGridListModule
+  ],
+  providers: [ElementService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
